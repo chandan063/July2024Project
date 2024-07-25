1bd133eb019a1180f900d033dbae67db82767109
chandan kumar <officialchandan063@gmail.com>
2024-07-25 15:12:12 +0530

    new changes

---
+++ index.js
@@ -0,0 +1,13 @@
+// document.getElementById('').id;
+// document.getElementById('').className;
+// document.getElementById('').getAttribute('id')
+// document.getElementById('').getAttribute('class')
+
+// document.getElementById('title1').setAttribute('class','help')
+//-----> it will set new class and overeride the old one
+
+// document.getElementById('title1').setAttribute('class','help') for setting to class name together
+const title=document.getElementById('title1');
+title.style.backgroundColor='green';
+title.style.padding='60px';
+title.style.borderRadius='15px';
